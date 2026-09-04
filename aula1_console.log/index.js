6e88d31c1acbfe1fed4ef534956f691e3568eb6d
Robson Ferreira <robsonldfer@gmail.com>
2026-09-04 00:39:14 -0300

    aula1
    Console.log()

---
+++ aula1_console.log/index.js
@@ -0,0 +1,6 @@
+console.log("Hello, World!");
+console.log("Hello, 'World!'");
+console.log('Hello, "World!"');
+console.log("Hello, \\\"World!\\\"");
+console.log(123456);
+console.log(35, 7.85, 'Robson');
