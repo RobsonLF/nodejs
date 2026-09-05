e09f84d0762b1a7d3ef4cb315a9d8169dddd9cc6
Robson Ferreira <robsonldfer@gmail.com>
2026-09-05 01:52:17 -0300

    aula4
    Variaveis com Let

---
+++ aula4_variaveisLet/index.js
@@ -0,0 +1,9 @@
+let nomepai = 'Robson';
+let nomemae = 'Maria';
+let nomefilho = 'Eduardo';
+
+console.log(`${nomepai} nasceu em 1984`);
+console.log(`Em 2000 ${nomepai} conheceu ${nomemae}`);
+console.log(`${nomepai} casou-se com ${nomemae} em 2012`);
+console.log(`${nomemae} teve 1 filho com ${nomepai} em 2015`);
+console.log(`O filho de ${nomepai} se chama ${nomefilho}`);
