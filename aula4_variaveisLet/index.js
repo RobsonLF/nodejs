3f52d902bbf192cc6d5d1cca771607a27fc7a96c
Robson Ferreira <robsonldfer@gmail.com>
2026-09-07 00:01:18 -0300

    aula5 const

--- aula4_variaveisLet/index.js
+++ aula4_variaveisLet/index.js
@@ -1,3 +1,12 @@
+//Não podemos criar variáveis com palavras reservadas
+// Variáveis precisam ter nomes significativos
+// Não podemos começar o nome de uma variável com um número
+// Não podem conter espaços ou traços
+// Utilizamos camelCase 
+// Case-sensitive
+// Não podemos redeclarar variáveis com let
+// Não utilize var, sempre utilize let
+
 let nomepai = 'Robson';
 let nomemae = 'Maria';
 let nomefilho = 'Eduardo';
---
+++ aula5_constantes/index.js
@@ -0,0 +1,11 @@
+//Não podemos criar variáveis com palavras reservadas
+// Variáveis precisam ter nomes significativos
+// Não podemos começar o nome de uma variável com um número
+// Não podem conter espaços ou traços
+// Utilizamos camelCase 
+// Case-sensitive
+// Não podemos modificar o valor de variáveis const
+// Não utilize var, sempre utilize const
+
+const nomepai = 'Robson';
+console.log(`${nomepai} nasceu em 1984`);
