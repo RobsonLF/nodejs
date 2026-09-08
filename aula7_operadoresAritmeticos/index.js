cd4ca765b976ae2c42d83da36df9444fc50406ca
Robson Ferreira <robsonldfer@gmail.com>
2026-09-08 20:16:19 -0300

    Aula7 - Operadores

--- aula7_operadoresAritmeticos/index.js
+++ aula7_operadoresAritmeticos/index.js
@@ -13,6 +13,15 @@
  * 2. **                              #
  * 3. * / %                           #
  * 4. + -                             #
+ * Incrementar                        #
+ * variavel++                         #
+ * Decrementar                        #
+ * variavel--                         #
+ *                                    #
+ * Operador de atribuição             #
+ * variavel +=
+ * 
+ * 
 #######################################    
 */                                   
 
@@ -20,3 +29,18 @@ const num1 = 5;
 const num2 = 2;
 const num3 = 10;
 console.log((num1 + num2) * num3);
+
+const passo = 2;
+let contador = 0;
+/*
+contador += passo; // contador = contador + passo
+console.log(contador);
+*/
+contador += 2; // contador = contador + 2
+console.log(contador);
+
+contador += 2; // contador = contador + 2
+console.log(contador);
+
+contador += 2; // contador = contador + 2
+console.log(contador);
