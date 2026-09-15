3cca9ec1b3575a01d459a50d4b4fb3d3b1839f48
Robson Ferreira <robsonldfer@gmail.com>
2026-09-15 19:32:51 -0300

    arredondamentos de floats

--- aula10_Numbers/index.js
+++ aula10_Numbers/index.js
@@ -1,8 +1,6 @@
 let num1 = 1500;
 let num2 = 2.554651351356;
 
-
-
 //console.log(num1.toString() + num2.toString());
 // Representação binária
 console.log(num1.toString(2));
@@ -16,3 +14,24 @@ console.log(num2.toFixed(2));
 console.log(num2.toFixed(5));
 
 console.log(Number.isInteger(num1));
+
+//Padrão de precisão de casas decimais
+//IEEE 754-2008
+
+let num3 = 0.7;
+let num4 = 0.1;
+
+num3 += num4; //num3 = num3 + num4
+console.log(num3);
+num3 += num4;
+console.log(num3);
+num3 += num4;
+console.log(num3);
+
+num3 = Number(num3.toFixed(2));
+
+console.log(num3); // 1.0
+console.log(Number.isInteger(num3));
+
+
+
