b4427017e8eea2d7fb37851f52ab65f2262be6f2
Robson Ferreira <robsonldfer@gmail.com>
2026-09-14 21:24:59 -0300

    Create index.js

---
+++ aula9_strings/index.js
@@ -0,0 +1,18 @@
+let umastring = 'Uma string';
+let outrastring = "O rato roeu a roupa do rei de Roma";
+/*
+console.log(umastring.length); // 10
+console.log(umastring.charAt(5))
+
+console.log(umastring.indexOf('string')); // 4
+console.log(umastring.lastIndexOf('a')); // 7
+
+console.log(umastring.match(/[a-z]/g));
+console.log(umastring.search(/[a-z]/g));
+
+console.log(outrastring.replace(/r/g, '#'));
+console.log(outrastring.replace(/r/g, '#').toUpperCase());
+*/
+console.log(outrastring.length);
+
+console.log(outrastring.slice(2, 6));
